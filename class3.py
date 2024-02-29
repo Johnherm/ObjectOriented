0dd13979895a45569f16749083c2958725507d18
Yohannes <johnherma72b@gmail.com>
2024-02-29 11:14:03 -0900

    Their is a change in def init if we change the variable name

--- class3.py
+++ class3.py
@@ -1,12 +1,11 @@
 class Employee():
-    def __init__(self,first,last,email,pay):
-        self.first = first
-        self.last = last
-        self.email = first + "." + last + "@gmail.com" # or just write email
-        self.pay = pay
+    def __init__(self,a,b,email,c):
+        self.first = a
+        self.last = b
+        self.email = a + "." + b + "@gmail.com" # or just write email
+        self.pay = c
 emp_1 = Employee("Corey","Schafer","Corey.Schafer@gmail.com",50000) #This is first employee data
-emp_2 = Employee("Test","User","Test.user@gmail.com",60000) #This is second employee data
-# #Printing out employees information
+emp_2 = Employee("Test","User","Test.user@gmail.com",60000) #This is second employee data# #Printing out employees information
 print("Printing out each information of employee")
 print("") # This is just for space
 print(f"first name of first employee is :  {emp_1.first}")
@@ -22,3 +21,18 @@ print(f"Salary of {emp_2.first} is : {emp_2.pay}")
 print("Printing out all information of employee at once ")
 print(f"{emp_1.first, emp_1.last, emp_1.email, emp_1.pay}")
 print(f"{emp_2.first, emp_2.last, emp_2.email, emp_2.pay}")
+# def firstEmployee():
+#     first = "Corey"
+#     last = "Schafer"
+#     email = "coery.schafer@gmail.com"
+#     pay =  50000
+#     print(first, last, email, pay)
+# def secondEmployee():
+#     first = "Test"
+#     last = "User"
+#     email = "test.user@gmail.com"
+#     pay = 60000
+#     # print(first, last, email, pay)
+#     print(first, last, email, pay)
+# firstEmployee()
+# secondEmployee()
