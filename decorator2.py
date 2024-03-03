d593d6f024b552b4eb0ffd8cb7dce9af59acf0b9
Yohannes <johnherma72b@gmail.com>
2024-03-03 14:20:27 -0900

    Summary in decorators

--- decorator2.py
+++ decorator2.py
@@ -1,11 +1,16 @@
 def decorator_function(original_function):
-    def wrapper_function():
-        return original_function()
+    def wrapper_function(*args, **kwargs):
+        return original_function(*args, **kwargs)
     return wrapper_function
 @decorator_function  # = decorated_display = decorator_function(display)
 def display():
     print("display function ran ")
 display()
+@decorator_function
+def display_info(name,age):
+    print(f"display info ran with arguments {name, age}")
+display_info('John',25)
+
 
 decorated_display = decorator_function(display)
 decorated_display()
