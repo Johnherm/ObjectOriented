e678a229a607e949526df8baf0c8c89d3918c86b
Yohannes <johnherma72b@gmail.com>
2024-03-05 18:16:39 -0900

    Class method

---
+++ class7.py
@@ -0,0 +1,30 @@
+class Employee():
+    num_of_employees = 0
+    raise_amt = 1.04
+    def __init__(just, first, last,pay):
+        just.fname = first
+        just.lname = last
+        just.email = first + "."+ last + "@gmail.com"
+        just.annual = pay
+        Employee.num_of_employees = Employee.num_of_employees + 1
+    def full_name(just):
+        return f"{just.fname, just.lname}" 
+    def apply_raise(just):
+        just.pay = int(just.pay* just.raise_amt)
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amt = amount
+
+
+
+emp_1 = Employee("Corey", "Jackson",50000)
+emp_2 = Employee("Test", "Employee",60000)
+
+emp_1.set_raise_amt(1.06) 
+Employee.set_raise_amt(1.05)
+print(emp_1.full_name())
+print(emp_2.full_name())
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
+print(Employee.num_of_employees)
